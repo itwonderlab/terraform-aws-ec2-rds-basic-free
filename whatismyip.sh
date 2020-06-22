587f2ee12d27afe97f3d6611d9a4085d9761c9b6
Javier Ruiz Jimenez <jruiz@itwonderlab.com>
2020-06-23 01:17:03 +0200

    Update Copyright to 2020

--- whatismyip.sh
+++ whatismyip.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# Copyright (C) 2018 IT Wonder Lab (https://www.itwonderlab.com)
+# Copyright (C) 2018 - 2020 IT Wonder Lab (https://www.itwonderlab.com)
 #
 # This software may be modified and distributed under the terms
 # of the MIT license.  See the LICENSE file for details.
